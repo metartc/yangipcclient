22dcbc147a6b068e40e5414abfa3623e9afaa4d7
yangrtc <yangrtc@outlook.com>
2025-11-02 13:05:11 +0800

    8.0-b0

--- mac/yangipcclient8/build/unknown-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_yangipcclient8_FILE /Users/yang/app/yangclient/mac/bin/app_debug/yangipcclient8.app/Contents/MacOS/yangipcclient8)
-set(__QT_DEPLOY_TARGET_yangipcclient8_TYPE EXECUTABLE)
