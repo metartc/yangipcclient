ab8fed0874b4dfca56a97c3e5357a60dc54a0726
yangrtc <yangrtc@outlook.com>
2026-06-21 11:21:11 +0800

    v8.0-b2

--- html/js/ipc_hd.mqtt.sdk.js
+++ html/js/ipc_hd.mqtt.sdk.js
@@ -17,114 +17,9 @@ function handleAnswer(msg){
 		
 }
 
-
-function yang_fill_query(query_string, obj) {
-            // pure user query object.
-            obj.user_query = {};
-
-            if (query_string.length === 0) {
-                return;
-            }
-
-            // split again for angularjs.
-            if (query_string.indexOf("?") >= 0) {
-                query_string = query_string.split("?")[1];
-            }
-
-            var queries = query_string.split("&");
-            for (var i = 0; i < queries.length; i++) {
-                var elem = queries[i];
-
-                var query = elem.split("=");
-                obj[query[0]] = query[1];
-                obj.user_query[query[0]] = query[1];
-            }
-
-            // alias domain for vhost.
-            if (obj.domain) {
-                obj.vhost = obj.domain;
-            }
- }
-
-function yang_url_parse(url) {
-            // @see: http://stackoverflow.com/questions/10469575/how-to-use-location-object-to-parse-url-without-redirecting-the-page-in-javascri
-            var a = document.createElement("a");
-            a.href = url.replace("rtmp://", "http://")
-                .replace("webrtc://", "https://")
-                .replace("rtc://", "http://");
-
-            var vhost = a.hostname.replace("[","").replace("]","");
-            var app = a.pathname.substr(1, a.pathname.lastIndexOf("/") - 1);
-            var stream = a.pathname.substr(a.pathname.lastIndexOf("/") + 1);
-			//console.log("host: ", vhost);
-            // parse the vhost in the params of app, that srs supports.
-            app = app.replace("...vhost...", "?vhost=");
-            if (app.indexOf("?") >= 0) {
-                var params = app.substr(app.indexOf("?"));
-                app = app.substr(0, app.indexOf("?"));
-
-                if (params.indexOf("vhost=") > 0) {
-                    vhost = params.substr(params.indexOf("vhost=") + "vhost=".length);
-                    if (vhost.indexOf("&") > 0) {
-                        vhost = vhost.substr(0, vhost.indexOf("&"));
-                    }
-                }
-            }
-
-            // when vhost equals to server, and server is ip,
-            // the vhost is __defaultVhost__
-            if (a.hostname === vhost) {
-                var re = /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/;
-                if (re.test(a.hostname)) {
-                    vhost = "__defaultVhost__";
-                }
-            }
-
-            // parse the schema
-            var schema = "rtmp";
-            if (url.indexOf("://") > 0) {
-                schema = url.substr(0, url.indexOf("://"));
-            }
-
-            var port = a.port;
-            if (!port) {
-                if (schema === 'http') {
-                    port = 80;
-                } else if (schema === 'https') {
-                    port = 443;
-                } else if (schema === 'rtmp') {
-                    port = 1935;
-                }
-            }
-
-            var ret = {
-                url: url,
-                schema: schema,
-                server: a.hostname, port: port,
-                vhost: vhost, app: app, stream: stream
-            };
-            yang_fill_query(a.search, ret);
-
-            // For webrtc API, we use 443 if page is https, or schema specified it.
-            if (!ret.port) {
-                if (schema === 'webrtc' || schema === 'rtc') {
-                    if (ret.user_query.schema === 'https') {
-                        ret.port = 443;
-                    } else if (window.location.href.indexOf('https://') === 0) {
-                        ret.port = 443;
-                    } else {
-                        // For WebRTC, SRS use 1985 as default API port.
-                        ret.port = 1985;
-                    }
-                }
-            }
-
-            return ret;
-        }
-
 // Depends on adapter-7.4.0.min.js from https://github.com/webrtc/adapter
 // Async-await-promise based SRS RTC Player.
-function SrsRtcPlayerAsync() {	
+function YangRtcPlayerAsync() {	
     var self = {};
     if(yang_mediaServerType==Yang_ConnectType_P2p){
 		//self.pc = new RTCPeerConnection({iceServers: yang_iceServer,iceTransportPolicy: 'relay'});
@@ -156,8 +51,6 @@ function SrsRtcPlayerAsync() {
 		}
     };
 	
-	//console.log("yang_mediaServerType==",yang_mediaServerType);
-	if(yang_mediaServerType==Yang_ConnectType_Srs) yang_hasDatachannel=false;
 	if(yang_hasDatachannel){
 	
 			datachannel=self.pc.createDataChannel('chat');
@@ -179,18 +72,29 @@ function SrsRtcPlayerAsync() {
 
 	
 
-	self.answerPlay = function(remoteSdp) {
-        self.pc.addTransceiver("audio", {direction: "recvonly"});
+	self.answerPlay = async function(remoteSdp) {
+        //self.pc.addTransceiver("audio", {direction: "sendrecv"});
+        //self.pc.addTransceiver("audio", {direction: "recvonly"});
         self.pc.addTransceiver("video", {direction: "recvonly"});	
-		self.pc.setRemoteDescription(new RTCSessionDescription({type: 'offer', sdp: remoteSdp}));
+        var stream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true});
+		stream.getTracks().forEach(function (track) {
+            self.pc.addTrack(track,stream);
+        });
+        console.log("remote sdp:\n"+remoteSdp);
+		await self.pc.setRemoteDescription(new RTCSessionDescription({type: 'offer', sdp: remoteSdp}));
   
-        sdk.pc.createAnswer().then(
-            function(description) {
-                self.pc.setLocalDescription(description);
-                sendMqttMsg(Yang_Request_Answer,null,description.sdp+"");
-            }, yang_trace
-        );
-      
+		const answer = await self.pc.createAnswer();
+		     
+		await self.pc.setLocalDescription(answer);
+		
+		 var session = await new Promise(function(resolve, reject) {    	 
+			console.log("answer sdp:\n"+answer.sdp);
+           sendMqttMsg(Yang_Request_Answer,null,answer.sdp+"");
+        });
+        
+        session.simulator = conf.schema + '//' + conf.urlObject.server + ':' + conf.port + '/rtc/v1/nack/';
+
+        return session;
     };
 
 
@@ -289,34 +193,3 @@ function SrsRtcPlayerAsync() {
 
     return self;
 }
-
-// Format the codec of RTCRtpSender, kind(audio/video) is optional filter.
-// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/WebRTC_codecs#getting_the_supported_codecs
-function SrsRtcFormatSenders(senders, kind) {
-    var codecs = [];
-    senders.forEach(function (sender) {
-        var params = sender.getParameters();
-        params && params.codecs && params.codecs.forEach(function(c) {
-            if (kind && sender.track.kind !== kind) {
-                return;
-            }
-
-            if (c.mimeType.indexOf('/red') > 0 || c.mimeType.indexOf('/rtx') > 0 || c.mimeType.indexOf('/fec') > 0) {
-                return;
-            }
-
-            var s = '';
-
-            s += c.mimeType.replace('audio/', '').replace('video/', '');
-            s += ', ' + c.clockRate + 'HZ';
-            if (sender.track.kind === "audio") {
-                s += ', channels: ' + c.channels;
-            }
-            s += ', pt: ' + c.payloadType;
-
-            codecs.push(s);
-        });
-    });
-    return codecs.join(", ");
-}
-
--- html_freertos/js/ipc_hd.mqtt.sdk.js
+++ html_freertos/js/ipc_hd.mqtt.sdk.js
@@ -17,114 +17,9 @@ function handleAnswer(msg){
 		
 }
 
-
-function yang_fill_query(query_string, obj) {
-            // pure user query object.
-            obj.user_query = {};
-
-            if (query_string.length === 0) {
-                return;
-            }
-
-            // split again for angularjs.
-            if (query_string.indexOf("?") >= 0) {
-                query_string = query_string.split("?")[1];
-            }
-
-            var queries = query_string.split("&");
-            for (var i = 0; i < queries.length; i++) {
-                var elem = queries[i];
-
-                var query = elem.split("=");
-                obj[query[0]] = query[1];
-                obj.user_query[query[0]] = query[1];
-            }
-
-            // alias domain for vhost.
-            if (obj.domain) {
-                obj.vhost = obj.domain;
-            }
- }
-
-function yang_url_parse(url) {
-            // @see: http://stackoverflow.com/questions/10469575/how-to-use-location-object-to-parse-url-without-redirecting-the-page-in-javascri
-            var a = document.createElement("a");
-            a.href = url.replace("rtmp://", "http://")
-                .replace("webrtc://", "https://")
-                .replace("rtc://", "http://");
-
-            var vhost = a.hostname.replace("[","").replace("]","");
-            var app = a.pathname.substr(1, a.pathname.lastIndexOf("/") - 1);
-            var stream = a.pathname.substr(a.pathname.lastIndexOf("/") + 1);
-			//console.log("host: ", vhost);
-            // parse the vhost in the params of app, that srs supports.
-            app = app.replace("...vhost...", "?vhost=");
-            if (app.indexOf("?") >= 0) {
-                var params = app.substr(app.indexOf("?"));
-                app = app.substr(0, app.indexOf("?"));
-
-                if (params.indexOf("vhost=") > 0) {
-                    vhost = params.substr(params.indexOf("vhost=") + "vhost=".length);
-                    if (vhost.indexOf("&") > 0) {
-                        vhost = vhost.substr(0, vhost.indexOf("&"));
-                    }
-                }
-            }
-
-            // when vhost equals to server, and server is ip,
-            // the vhost is __defaultVhost__
-            if (a.hostname === vhost) {
-                var re = /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/;
-                if (re.test(a.hostname)) {
-                    vhost = "__defaultVhost__";
-                }
-            }
-
-            // parse the schema
-            var schema = "rtmp";
-            if (url.indexOf("://") > 0) {
-                schema = url.substr(0, url.indexOf("://"));
-            }
-
-            var port = a.port;
-            if (!port) {
-                if (schema === 'http') {
-                    port = 80;
-                } else if (schema === 'https') {
-                    port = 443;
-                } else if (schema === 'rtmp') {
-                    port = 1935;
-                }
-            }
-
-            var ret = {
-                url: url,
-                schema: schema,
-                server: a.hostname, port: port,
-                vhost: vhost, app: app, stream: stream
-            };
-            yang_fill_query(a.search, ret);
-
-            // For webrtc API, we use 443 if page is https, or schema specified it.
-            if (!ret.port) {
-                if (schema === 'webrtc' || schema === 'rtc') {
-                    if (ret.user_query.schema === 'https') {
-                        ret.port = 443;
-                    } else if (window.location.href.indexOf('https://') === 0) {
-                        ret.port = 443;
-                    } else {
-                        // For WebRTC, SRS use 1985 as default API port.
-                        ret.port = 1985;
-                    }
-                }
-            }
-
-            return ret;
-        }
-
 // Depends on adapter-7.4.0.min.js from https://github.com/webrtc/adapter
 // Async-await-promise based SRS RTC Player.
-function SrsRtcPlayerAsync() {	
+function YangRtcPlayerAsync() {	
     var self = {};
     if(yang_mediaServerType==Yang_ConnectType_P2p){
 		//self.pc = new RTCPeerConnection({iceServers: yang_iceServer,iceTransportPolicy: 'relay'});
@@ -156,8 +51,6 @@ function SrsRtcPlayerAsync() {
 		}
     };
 	
-	//console.log("yang_mediaServerType==",yang_mediaServerType);
-	if(yang_mediaServerType==Yang_ConnectType_Srs) yang_hasDatachannel=false;
 	if(yang_hasDatachannel){
 	
 			datachannel=self.pc.createDataChannel('chat');
@@ -300,34 +193,3 @@ function SrsRtcPlayerAsync() {
 
     return self;
 }
-
-// Format the codec of RTCRtpSender, kind(audio/video) is optional filter.
-// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/WebRTC_codecs#getting_the_supported_codecs
-function SrsRtcFormatSenders(senders, kind) {
-    var codecs = [];
-    senders.forEach(function (sender) {
-        var params = sender.getParameters();
-        params && params.codecs && params.codecs.forEach(function(c) {
-            if (kind && sender.track.kind !== kind) {
-                return;
-            }
-
-            if (c.mimeType.indexOf('/red') > 0 || c.mimeType.indexOf('/rtx') > 0 || c.mimeType.indexOf('/fec') > 0) {
-                return;
-            }
-
-            var s = '';
-
-            s += c.mimeType.replace('audio/', '').replace('video/', '');
-            s += ', ' + c.clockRate + 'HZ';
-            if (sender.track.kind === "audio") {
-                s += ', channels: ' + c.channels;
-            }
-            s += ', pt: ' + c.payloadType;
-
-            codecs.push(s);
-        });
-    });
-    return codecs.join(", ");
-}
-
